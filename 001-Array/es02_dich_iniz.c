ed76327aeddfd27258a822acfb32abc6680f2ff7
Filippo <filippo.bilardo@tiscali.it>
2022-10-05 09:32:23 +0000

    Dichiarazione e inizializzazione array

--- 001-Array/es02_dich_iniz.c
+++ 001-Array/es02_dich_iniz.c
@@ -8,17 +8,24 @@ int main(void)
 	//Dichiarazione e assegnazione di valori al vettore di interi vet
 	//Questo metodo di assegnazione si può fare solo in fase di dichiarazione
 	//I valori del vettore sono inizializzati tutti a zero
-	int vet[5]={1, 2, 3};
+	int vet_parz_iniz[10]={4, 1, 3};
+	int vet_non_iniz[10];
 
-	//Stampa a schermo dei valori di vet[]
-	printf("\nStampa a schermo dei valori di vet[]\n");
-	for(int i=0;i<5;i++) {
-		printf("vet[%d]= %d\n",i , vet[i]);
+	//Stampa a schermo dei valori di dati[]
+	printf("\nStampa a schermo dei valori di vet_parz_iniz[]\n");
+	for(int i=0;i<10;i++) {
+		printf("vet_parz_iniz[%d]= %d\n",i , vet_parz_iniz[i]);
+	}
+
+	//Stampa a schermo dei valori di vet_non_iniz[]
+	printf("\nStampa a schermo dei valori di vet_non_iniz[]\n");
+	for(int i=0;i<10;i++) {
+		printf("vet_non_iniz[%d]= %d\n",i , vet_non_iniz[i]);
 	}
 
 	//Dichiarazione
 	float vet2[3];
-	//inizializzazione dei valori
+	//inizializzazione dei valori successiva alla dichiarazione
 	//vet2[3]={1.0, 2.2, 9.3}; //Errore
 	vet2[0]=1.0;
 	vet2[1]=2.2;
