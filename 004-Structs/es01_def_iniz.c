bd9f959ffc03521d09225ac6ded7cf241ca29d6a
Filippo <filippo.bilardo@tiscali.it>
2022-10-26 09:56:37 +0000

    update

--- 004-Structs/es01_def_iniz.c
+++ 004-Structs/es01_def_iniz.c
@@ -8,27 +8,33 @@ struct indirizzo {
   int civico; 
 };
 
+#define REC_IND3 3  //Numero di record della tabella ind3
+
 int main() {
   // Creiamo la variabile strutture e assegnamo dei valori
-  struct myStructure ind1 = {13, 'B', "Some text"};
+  struct indirizzo ind1 = {"Arese", "via Matteotti", 12};
   // Solo dicharazione
-  struct myStructure ind2;
+  struct indirizzo ind2;
+  struct indirizzo ind3[REC_IND3]; //ind3 è una tabella, ogni elem. è un record
 
-  ind2 = {13, 'B', "Some text"};
+  //ind2.citta = "Arese"; //error: assignment to expression with array type
+  //ind2.via = "via Milano"; //error: assignment to expression with array type
+  //ind2.citta = ind1.citta; //error: assignment to expression with array type
+  strcpy(ind2.citta, ind1.citta);
+  strcpy(ind2.via, "via Milano");
+  ind2.civico = 57;
 
-  s2=s1;
-  // Print values
-  printf("%d %c %s", s2.myNum, s2.myLetter, s2.myString);
-  
-  // Modify values
-  s1.myNum = 30;
-  s1.myLetter = 'C';
-  strcpy(s1.myString, "Something else");
-  s2=s1;
-  s2.myNum = 32;
+  //per i campi stringa siamo obbligati ad usare la strcpy
+  strcpy(ind3[0].citta, "Bollate");
+  strcpy(ind3[0].via, "via Monza");
+  ind3[0].civico = ind1.civico;
 
-  // Print values
-  printf("%d %c %s", s2.myNum, s2.myLetter, s2.myString);
+  //Stampiamo il contenuto dei record
+  printf("Citta: %s - %s, %d\n", ind1.citta, ind1.via, ind1.civico);
+  printf("Citta: %s - %s, %d\n", ind2.citta, ind2.via, ind2.civico);
+  for(int i=0; i<REC_IND3; i++) {
+    printf("Citta: %s - %s, %d\n", ind3[i].citta, ind3[i].via, ind3[i].civico);
+  }
 
   return 0;
 }
